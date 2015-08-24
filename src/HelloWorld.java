1592e695524c134d4b83a24a2084c495e03e1fa3
Xuefei Zhang <xuezhang@expedia.com>
2015-08-24 10:16:41 -0700

    hello world

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+package src;
+
+public class HelloWorld {
+
+    public static void sayHelloWorld() {
+        System.out.println("Hello World!");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,7 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
-        String letter = "o";
-        String s = "hello"; // These two String objects
-        String t = "hell" + letter; // contain exactly the same text.
-        if (s.equals(t)) {
-            System.out.println("equal");
-        }
+        HelloWorld.sayHelloWorld();
 
     }
 }
