7f39e87db550cac037d7949371934610fbc02f8a
Xuefei Zhang <wo00faye@gmail.com>
2016-08-19 11:56:33 -0700

    leetcode

---
+++ src/leetcode/LeetCode102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,66 @@
+/**
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * 
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its level order traversal as:
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ */
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import leetcode.util.TreeNode;
+
+public class LeetCode102BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<TreeNode> currentLevel = new ArrayList<>();
+
+        if (null == root) {
+            return result;
+        }
+        currentLevel.add(root);
+
+        levelOrderHelper(currentLevel, result);
+
+        return result;
+    }
+
+    private void levelOrderHelper(List<TreeNode> currentLevel, List<List<Integer>> result) {
+
+        if (null == currentLevel || currentLevel.isEmpty()) {
+            return;
+        }
+
+        List<Integer> currentLevelInteger = new ArrayList<>();
+        List<TreeNode> nextLevel = new ArrayList<>();
+
+        for (int i = 0; i < currentLevel.size(); i++) {
+            if (null != currentLevel.get(i)) {
+                currentLevelInteger.add(currentLevel.get(i).val);
+            }
+            if (null != currentLevel.get(i).left) {
+                nextLevel.add(currentLevel.get(i).left);
+            }
+            if (null != currentLevel.get(i).right) {
+                nextLevel.add(currentLevel.get(i).right);
+            }
+        }
+
+        result.add(currentLevelInteger);
+        levelOrderHelper(nextLevel, result);
+    }
+}
