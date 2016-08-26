56233c87f4cd4c30e468777958c6a6ab287c191f
Xuefei Zhang <wo00faye@gmail.com>
2016-08-26 15:56:23 -0700

    leetcode

--- src/leetcode/LeetCode009PalindromeNumber.java
+++ src/leetcode/LeetCode009PalindromeNumber.java
@@ -11,16 +11,12 @@ public class LeetCode009PalindromeNumber {
             return false;
         }
 
-        if (x == 0) {
-            return true;
-        }
-
         int div = 1;
         while (x / div >= 10) {
             div = div * 10;
         }
 
-        while (x >= 10) {
+        while (x != 0) {
 
             if (x % 10 != x / div) {
                 return false;
@@ -28,13 +24,6 @@ public class LeetCode009PalindromeNumber {
 
             x = (x % div) / 10;
             div = div / 100;
-        }
-
-        if (x == 0) {
-            return true;
-        }
-        if (x < div) {
-            return false;
         }
 
         return true;
