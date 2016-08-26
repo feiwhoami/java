b1b96e47aa5f539deccbe41f3a20e1c72c8dd5fa
Xuefei Zhang <wo00faye@gmail.com>
2016-08-26 15:22:49 -0700

    leetcode

--- src/leetcode/LeetCode027RemoveElement.java
+++ src/leetcode/LeetCode027RemoveElement.java
@@ -40,4 +40,21 @@ public class LeetCode027RemoveElement {
 
         return first;
     }
+
+    public int removeElement2(int[] nums, int val) {
+
+        if (null == nums || 0 == nums.length) {
+            return 0;
+        }
+
+        int i = 0, j = 0;
+        for (; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
 }
