e8b05fc65b5de26ab476e93558e54eaa2968f69a
feiwhoami <wo00faye@gmail.com>
2016-08-29 20:59:12 -0700

    leetcode

---
+++ src/leetcode/LeetCode058LengthofLastWord.java
@@ -0,0 +1,36 @@
+/**
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ * 
+ * If the last word does not exist, return 0.
+ * 
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ * 
+ * For example, 
+ * Given s = "Hello World",
+ * return 5.
+ */
+package leetcode;
+
+public class LeetCode058LengthofLastWord {
+
+    public int lengthOfLastWord(String s) {
+
+        if (null == s || s.isEmpty()) {
+            return 0;
+        }
+
+        int i = s.length() - 1;
+        int length = 0;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
