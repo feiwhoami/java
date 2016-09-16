1f95b21bec1b13a2e0cb06da048a8061b7ede829
feiwhoami <wo00faye@gmail.com>
2016-09-16 15:19:56 -0700

    leetcode

---
+++ src/leetcode/LeetCode50Powxn.java
@@ -0,0 +1,42 @@
+/**
+ * Implement pow(x, n).
+ */
+package leetcode;
+
+public class LeetCode50Powxn {
+
+    public double myPow(double x, int n) {
+
+        if (0 == x) {
+            return 0;
+        }
+
+        if (1 == x) {
+            return 1;
+        }
+
+        if (0 == n) {
+            return 1;
+        }
+
+        if (1 == n) {
+            return x;
+        }
+
+        if (n < 0) {
+            if (n == Integer.MIN_VALUE) {
+                return 1 / (x * myPow(x, Integer.MAX_VALUE));
+            }
+            return 1 / myPow(x, -n);
+        }
+
+        if (n % 2 == 0) {
+            double half = myPow(x, n / 2);
+            return half * half;
+        } else {
+            double half = myPow(x, n / 2);
+            return half * half * x;
+        }
+
+    }
+}
