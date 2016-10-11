4d03b100d45dad3f558dbf12fe932eb79d51ac76
feiwhoami <wo00faye@gmail.com>
2016-10-10 20:31:40 -0700

    leetcode

--- src/leetcode/LeetCode096UniqueBinarySearchTrees.java
+++ src/leetcode/LeetCode096UniqueBinarySearchTrees.java
@@ -32,7 +32,22 @@ public class LeetCode096UniqueBinarySearchTrees {
         }
 
         return list.get(n);
+    }
 
+    public int numTrees2(int n) {
+
+        int[] nums = new int[n + 1];
+
+        nums[0] = 1;
+        nums[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                nums[i] = nums[i] + nums[j] * nums[i - j - 1];
+            }
+        }
+
+        return nums[n];
     }
 
 }
