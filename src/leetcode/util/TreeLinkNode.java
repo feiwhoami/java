4a0de73c0cfb3dd068cc7bbb3349f13a1be4d5f8
feiwhoami <wo00faye@gmail.com>
2016-10-19 22:26:51 -0700

    leetcode

---
+++ src/leetcode/util/TreeLinkNode.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for binary tree with next pointer.
+ */
+package leetcode.util;
+
+public class TreeLinkNode {
+
+    public int val;
+
+    public TreeLinkNode left;
+
+    public TreeLinkNode right;
+
+    public TreeLinkNode next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
