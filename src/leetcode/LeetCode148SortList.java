770ceffac790a4c8f8d4faa072122884ece7e593
feiwhoami <wo00faye@gmail.com>
2016-11-02 11:44:46 -0700

    leetcode

---
+++ src/leetcode/LeetCode148SortList.java
@@ -0,0 +1,58 @@
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ */
+package leetcode;
+
+import leetcode.util.ListNode;
+
+public class LeetCode148SortList {
+
+    public ListNode sortList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 == null) {
+            curr.next = l2;
+        }
+        if (l2 == null) {
+            curr.next = l1;
+        }
+
+        return dummy.next;
+    }
+}
