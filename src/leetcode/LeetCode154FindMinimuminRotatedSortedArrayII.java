44c4e17aae3f4ba1ce82ffe11d4c594b326cbe78
feiwhoami <wo00faye@gmail.com>
2016-11-02 17:21:32 -0700

    leetcode

---
+++ src/leetcode/LeetCode154FindMinimuminRotatedSortedArrayII.java
@@ -0,0 +1,27 @@
+/**
+ * Follow up for "Find Minimum in Rotated Sorted Array":
+ * What if duplicates are allowed?
+ * 
+ * Would this affect the run-time complexity? How and why?
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * Find the minimum element.
+ * 
+ * The array may contain duplicates.
+ */
+package leetcode;
+
+public class LeetCode154FindMinimuminRotatedSortedArrayII {
+
+    public int findMin(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return nums[i + 1];
+            }
+        }
+        return nums[0];
+    }
+}
