550deb938b5562c7ac6482a251f9276bbc6ff6c9
feiwhoami <wo00faye@gmail.com>
2016-11-23 14:18:15 -0800

    leetcode

---
+++ src/leetcode/LeetCode213HouseRobberII.java
@@ -0,0 +1,50 @@
+/**
+ * Note: This is an extension of House Robber.
+ * 
+ * After robbing those houses on that street, the thief has found himself a new place for his thievery so that he will not get too much attention. This time, all houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, the security system for these houses remain the same as for those in the previous street.
+ * 
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+ */
+package leetcode;
+
+public class LeetCode213HouseRobberII {
+
+    public int rob(int[] nums) {
+
+        if (null == nums || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int result = 0;
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        if (nums.length == 2) {
+            return dp[1];
+        }
+
+        for (int i = 2; i < nums.length - 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        result = dp[nums.length - 2];
+
+        dp[1] = nums[1];
+        dp[2] = Math.max(nums[1], nums[2]);
+
+        for (int i = 3; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        result = Math.max(result, dp[nums.length - 1]);
+
+        return result;
+    }
+
+}
