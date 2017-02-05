b337820c2dae9002f80aa89fc877cead43e75fa1
feiwhoami <wo00faye@gmail.com>
2017-02-04 21:27:59 -0800

    leetcode

---
+++ src/leetcode/LeetCode295FindMedianfromDataStream.java
@@ -0,0 +1,51 @@
+/**
+ * Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+ * 
+ * Examples: 
+ * [2,3,4] , the median is 3
+ * 
+ * [2,3], the median is (2 + 3) / 2 = 2.5
+ * 
+ * Design a data structure that supports the following two operations:
+ * 
+ * void addNum(int num) - Add a integer number from the data stream to the data structure.
+ * double findMedian() - Return the median of all elements so far.
+ * For example:
+ * 
+ * addNum(1)
+ * addNum(2)
+ * findMedian() -> 1.5
+ * addNum(3) 
+ * findMedian() -> 2
+ */
+package leetcode;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LeetCode295FindMedianfromDataStream {
+    PriorityQueue<Integer> min;
+    PriorityQueue<Integer> max;
+
+    /** initialize your data structure here. */
+    public LeetCode295FindMedianfromDataStream() {
+        min = new PriorityQueue<Integer>();
+        max = new PriorityQueue<Integer>(1, Collections.reverseOrder());
+    }
+
+    public void addNum(int num) {
+        max.add(num);
+        min.add(max.poll());
+        if (max.size() < min.size()) {
+            max.add(min.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (max.size() == min.size()) {
+            return (double)(max.peek() + min.peek()) / 2;
+        } else {
+            return max.peek();
+        }
+    }
+}
