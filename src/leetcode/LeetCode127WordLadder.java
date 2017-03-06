b0daab9c5c0e4ed7951d2775a956ea5f432a4716
feiwhoami <wo00faye@gmail.com>
2017-03-05 16:12:00 -0800

    leetcode

--- src/leetcode/LeetCode127WordLadder.java
+++ src/leetcode/LeetCode127WordLadder.java
@@ -20,11 +20,15 @@
 package leetcode;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
 public class LeetCode127WordLadder {
 
+    /**
+     * Solution 1
+     */
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
 
         if (null == beginWord || null == endWord || beginWord.length() == 0 || endWord.length() == 0 || null == wordList
@@ -70,6 +74,54 @@ public class LeetCode127WordLadder {
             }
         }
 
+        return 0;
+    }
+
+    /**
+     * Solution 2
+     */
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+        if (null == beginWord || beginWord.isEmpty() || null == endWord || endWord.isEmpty() || null == wordList
+                || wordList.isEmpty() || !wordList.contains(endWord)) {
+            return 0;
+        }
+
+        int level = 1;
+        int currNum = 1;
+        int nextNum = 0;
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+
+        while (!queue.isEmpty()) {
+            String currWord = queue.poll();
+            currNum--;
+
+            for (int i = 0; i < currWord.length(); i++) {
+                char[] currArray = currWord.toCharArray();
+
+                for (char c = 'a'; c <= 'z'; c++) {
+                    currArray[i] = c;
+                    String nextWord = new String(currArray);
+
+                    if (endWord.equals(nextWord)) {
+                        return level + 1;
+                    }
+
+                    if (wordList.contains(nextWord)) {
+                        queue.add(nextWord);
+                        nextNum++;
+                        wordList.remove(nextWord);
+                    }
+                }
+            }
+
+            if (currNum == 0) {
+                level++;
+                currNum = nextNum;
+                nextNum = 0;
+            }
+        }
         return 0;
     }
 }
