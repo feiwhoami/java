0bf93cb23d4f7c9b0613849085b50055ffb32639
Xuefei Zhang <wo00faye@gmail.com>
2017-09-25 09:54:05 -0700

    revert main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,9 +7,6 @@ import leetcode.*;
 public class Main {
 
     public static void main(String[] args) {
-        LeetCode557ReverseWordsinaStringIII solution = new LeetCode557ReverseWordsinaStringIII();
-
-        String s = solution.reverseWords("Let's take LeetCode contest");
-        System.out.println(s);
+        HelloWorld.sayHelloWorld();
     }
 }
