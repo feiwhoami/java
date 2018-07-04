090fd65548e576d7bfa9a099794ae44680740612
Xuefei Zhang <wo00faye@gmail.com>
2018-07-04 16:22:26 -0700

    leetcode

--- src/leetcode/LeetCode098ValidateBinarySearchTree.java
+++ src/leetcode/LeetCode098ValidateBinarySearchTree.java
@@ -1,20 +1,22 @@
 /**
  * Given a binary tree, determine if it is a valid binary search tree (BST).
- * 
+ * <p>
  * Assume a BST is defined as follows:
- * 
- * The left subtree of a node contains only nodes with keys less than the node's key.
- * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * <p>
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
  * Both the left and right subtrees must also be binary search trees.
  * Example 1:
- *     2
- *    / \
- *   1   3
+ * 2
+ * / \
+ * 1   3
  * Binary tree [2,1,3], return true.
  * Example 2:
- *     1
- *    / \
- *   2   3
+ * 1
+ * / \
+ * 2   3
  * Binary tree [1,2,3], return false.
  */
 package leetcode;
@@ -36,7 +38,7 @@ public class LeetCode098ValidateBinarySearchTree {
         if (root.val < min || root.val > max) {
             return false;
         }
-
-        return helper(root.left, min, Long.valueOf(root.val)) && helper(root.right, Long.valueOf(root.val), max);
+        return helper(root.left, min, Long.valueOf(root.val)) && helper(root
+                .right, Long.valueOf(root.val), max);
     }
 }
--- src/leetcode/LeetCode099RecoverBinarySearchTree.java
+++ src/leetcode/LeetCode099RecoverBinarySearchTree.java
@@ -1,10 +1,11 @@
 /**
  * Two elements of a binary search tree (BST) are swapped by mistake.
- * 
+ * <p>
  * Recover the tree without changing its structure.
- * 
+ * <p>
  * Note:
- * A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+ * A solution using O(n) space is pretty straight forward. Could you devise a
+ * constant space solution?
  */
 package leetcode;
 
