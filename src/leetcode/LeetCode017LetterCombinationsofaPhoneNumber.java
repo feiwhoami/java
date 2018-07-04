304d77323ab939e4a7f944933bf324e4c2b85467
Xuefei Zhang <wo00faye@gmail.com>
2018-07-04 11:47:41 -0700

    leetcode

--- src/leetcode/LeetCode017LetterCombinationsofaPhoneNumber.java
+++ src/leetcode/LeetCode017LetterCombinationsofaPhoneNumber.java
@@ -8,12 +8,23 @@
  */
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LeetCode017LetterCombinationsofaPhoneNumber {
 
+    private Map<Character, List<Character>> phoneKeys = new HashMap<>();
+
     public List<String> letterCombinations(String digits) {
+        phoneKeys.put('0', null);
+        phoneKeys.put('1', null);
+        phoneKeys.put('2', Arrays.asList('a', 'b', 'c'));
+        phoneKeys.put('3', Arrays.asList('d', 'e', 'f'));
+        phoneKeys.put('4', Arrays.asList('g', 'h', 'i'));
+        phoneKeys.put('5', Arrays.asList('j', 'k', 'l'));
+        phoneKeys.put('6', Arrays.asList('m', 'n', 'o'));
+        phoneKeys.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        phoneKeys.put('8', Arrays.asList('t', 'u', 'v'));
+        phoneKeys.put('9', Arrays.asList('w', 'x', 'y', 'z'));
 
         List<String> result = new ArrayList<>();
         if (null == digits || digits.isEmpty()) {
@@ -38,114 +49,15 @@ public class LeetCode017LetterCombinationsofaPhoneNumber {
 
         char c = digits.charAt(index);
 
-        if (c == '0') {
+        List<Character> keys = phoneKeys.get(c);
+        if (keys == null) {
             letterCombinationsHelper(digits, index + 1, curr, result);
-        } else if (c == '1') {
-            letterCombinationsHelper(digits, index + 1, curr, result);
-        } else if (c == '2') {
-            curr = curr + 'a';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'b';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'c';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '3') {
-            curr = curr + 'd';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'e';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'f';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '4') {
-            curr = curr + 'g';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'h';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'i';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '5') {
-            curr = curr + 'j';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'k';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'l';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '6') {
-            curr = curr + 'm';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'n';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'o';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '7') {
-            curr = curr + 'p';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'q';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'r';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 's';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '8') {
-            curr = curr + 't';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'u';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'v';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-        } else if (c == '9') {
-            curr = curr + 'w';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'x';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'y';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
-
-            curr = curr + 'z';
-            letterCombinationsHelper(digits, index + 1, curr, result);
-            curr = curr.substring(0, curr.length() - 1);
+        } else {
+            for (Character k : keys) {
+                curr = curr + k;
+                letterCombinationsHelper(digits, index + 1, curr, result);
+                curr = curr.substring(0, curr.length() - 1);
+            }
         }
     }
 }
