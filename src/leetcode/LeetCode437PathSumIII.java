67125091b80e55ca3f0aac43de87da943d6146eb
Xuefei Zhang <wo00faye@gmail.com>
2018-07-04 17:15:17 -0700

    leetcode

--- src/leetcode/LeetCode437PathSumIII.java
+++ src/leetcode/LeetCode437PathSumIII.java
@@ -1,16 +1,16 @@
 /**
  * You are given a binary tree in which each node contains an integer value.
- * 
+ *
  * Find the number of paths that sum to a given value.
- * 
+ *
  * The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
- * 
+ *
  * The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
- * 
+ *
  * Example:
- * 
+ *
  * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
- * 
+ *
  *       10
  *      /  \
  *     5   -3
@@ -18,40 +18,39 @@
  *   3   2   11
  *  / \   \
  * 3  -2   1
- * 
+ *
  * Return 3. The paths that sum to 8 are:
- * 
+ *
  * 1.  5 -> 3
  * 2.  5 -> 2 -> 1
  * 3. -3 -> 11
  */
 package leetcode;
 
+import leetcode.util.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import leetcode.util.TreeNode;
 
 public class LeetCode437PathSumIII {
 
     private int result = 0;
 
     public int pathSum(TreeNode root, int sum) {
-        if (null == root) {
+        if (root == null) {
             return 0;
         }
         List<Integer> currSum = new ArrayList<>();
-        pathSumHelper(root, sum, currSum);
+        helper(root, sum, currSum);
         return result;
     }
 
-    private void pathSumHelper(TreeNode root, int sum, List<Integer> currSum) {
-        if (null == root) {
+    private void helper(TreeNode root, int sum, List<Integer> currSum) {
+        if (root == null) {
             return;
         }
 
         List<Integer> newCurrSum = new ArrayList<>(currSum);
-
         newCurrSum.add(0);
         for (int i = 0; i < newCurrSum.size(); i++) {
             int tmp = newCurrSum.get(i) + root.val;
@@ -61,7 +60,7 @@ public class LeetCode437PathSumIII {
             newCurrSum.set(i, tmp);
         }
 
-        pathSumHelper(root.left, sum, newCurrSum);
-        pathSumHelper(root.right, sum, newCurrSum);
+        helper(root.left, sum, newCurrSum);
+        helper(root.right, sum, newCurrSum);
     }
 }
