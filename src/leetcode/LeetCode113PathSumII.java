9884a542cbd1f93be737752a8f973546fddb5f95
Xuefei Zhang <wo00faye@gmail.com>
2018-07-04 17:00:02 -0700

    leetcode

--- src/leetcode/LeetCode113PathSumII.java
+++ src/leetcode/LeetCode113PathSumII.java
@@ -1,47 +1,46 @@
 /**
- * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
- * 
+ * Given a binary tree and a sum, find all root-to-leaf paths where each
+ * path's sum equals the given sum.
+ * <p>
  * For example:
  * Given the below binary tree and sum = 22,
- *               5
- *              / \
- *             4   8
- *            /   / \
- *           11  13  4
- *          /  \    / \
- *         7    2  5   1
+ * 5
+ * / \
+ * 4   8
+ * /   / \
+ * 11  13  4
+ * /  \    / \
+ * 7    2  5   1
  * return
  * [
- *    [5,4,11,2],
- *    [5,8,4,5]
+ * [5,4,11,2],
+ * [5,8,4,5]
  * ]
  */
 package leetcode;
 
+import leetcode.util.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import leetcode.util.TreeNode;
 
 public class LeetCode113PathSumII {
 
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-
+        List<List<Integer>> result = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
 
-        pathSumHelper(root, curr, sum, result);
-
+        helper(root, curr, sum, result);
         return result;
     }
 
-    private void pathSumHelper(TreeNode root, List<Integer> curr, int target, List<List<Integer>> result) {
-        if (null == root) {
+    private void helper(TreeNode root, List<Integer> curr, int target,
+                        List<List<Integer>> result) {
+        if (root == null) {
             return;
         }
 
-        if (root.val == target && null == root.left && null == root.right) {
+        if (root.val == target && root.left == null && root.right == null) {
             curr.add(root.val);
             result.add(new ArrayList<Integer>(curr));
             curr.remove(curr.size() - 1);
@@ -49,8 +48,8 @@ public class LeetCode113PathSumII {
         }
 
         curr.add(root.val);
-        pathSumHelper(root.left, curr, target - root.val, result);
-        pathSumHelper(root.right, curr, target - root.val, result);
+        helper(root.left, curr, target - root.val, result);
+        helper(root.right, curr, target - root.val, result);
         curr.remove(curr.size() - 1);
     }
 }
